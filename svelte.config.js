d61fa3732edc9586b78cb1a62099622c2306072c
Billy Mellis <mellisbill@gmail.com>
2023-01-01 12:18:13 +0000

    Changed adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,17 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// default options are shown
-		adapter: adapter({
-		  // if true, will create a Netlify Edge Function rather
-		  // than using standard Node-based functions
-		  edge: true,
-	
-		  // if true, will split your app into multiple functions
-		  // instead of creating a single one for the entire app.
-		  // if `edge` is true, this option cannot be used
-		//   split: false
-		})
+		adapter: cloudflare()
 	  }
 };
 
